06505fdd4fdccf16bf9030b962073e4a79a5c638
BuchNieZyje <krzysowskyy@gmail.com>
2022-07-19 23:26:06 +0200

    Custom Prefix

--- src/main/java/me/buch/TipCommand.java
+++ src/main/java/me/buch/TipCommand.java
@@ -15,11 +15,11 @@ public class TipCommand implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-        String Prefix = ChatColor.GOLD + "[" + ChatColor.YELLOW + "RandomTip" + ChatColor.GOLD + "] ";
+        String Prefix = RandomTip.instance.getConfig().getString("Prefix");
         if(args.length == 0){
         List<String> tips = RandomTip.instance.getConfig().getStringList("tips");
         String Tips1 = tips.get(new Random().nextInt(tips.size()));
-        sender.sendMessage(Prefix + ChatColor.translateAlternateColorCodes('&',Tips1));
+        sender.sendMessage(Prefix+"" + ChatColor.translateAlternateColorCodes('&',Tips1));
         }
 
 
